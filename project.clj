88e01bfff997f23838e361296eab78d2b208a693
michniewicz <eldyrin@gmail.com>
2020-03-08 16:17:43 +0100

    Update auth0-lock to the latest version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
                  [org.clojure/clojurescript "1.10.597"]
                  [reagent "0.10.0"]
                  [re-frame "0.12.0"]
-                 [cljsjs/auth0-lock "10.16.0-0"]]
+                 [cljsjs/auth0-lock "11.16.2-0"]]
 
   :plugins [[lein-cljsbuild "1.1.4"]]
 
